d099c1ce351d764747f0174239f277427df780b8
Aswathy Asokan S <aswathyasokans99@gmail.com>
2023-07-26 20:50:24 +0530

    first commit.

---
+++ SampleAutomationDemo/src/main/java/std/Student.java
@@ -0,0 +1,12 @@
+package std;
+
+public class Student {
+	String stud_name="Aswathy";
+	int id=100;
+	String course="java";
+	public void display()
+	{
+		System.out.println(stud_name + " " + id + " "+ course);
+		}
+
+}
---
+++ SampleAutomationDemo/src/test/java/test/StudentDetails.java
@@ -0,0 +1,13 @@
+package test;
+import std.*;
+public class StudentDetails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student st=new Student();
+		st.display();
+
+
+	}
+
+}

31d37c60f41df7dac4ade32eea2cd13be0ee9868
Aswathy Asokan S <aswathyasokans99@gmail.com>
2023-07-28 22:38:12 +0530

    my second commit.

---
+++ SampleAutomationDemo/src/main/java/assigment/Divisible.java
@@ -0,0 +1,17 @@
+package assigment;
+
+public class Divisible {
+	int a;
+	int b;
+	public  Divisible(int a,int b)
+	{
+		this.a=a;
+		this.b=b;
+		
+	}
+	public int add()
+	{
+		return a+b;
+	}
+
+}

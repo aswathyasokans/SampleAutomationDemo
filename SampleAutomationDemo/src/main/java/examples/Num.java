97f9915017f4b2538e16d21149681909ca205f70
Aswathy Asokan S <aswathyasokans99@gmail.com>
2023-09-17 11:20:58 +0530

    num commit..

---
+++ SampleAutomationDemo/src/main/java/examples/Num.java
@@ -0,0 +1,64 @@
+package examples;
+
+public class Num {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/*int a=8;
+		if(a%2==0)
+		{
+			System.out.println("Even no");
+		}
+		else
+		{
+			System.out.println("not even");
+		}*/
+		/*int year =2000;
+		if(year%400==0)
+		{
+			System.out.println("leap year");
+		}
+		else {
+			System.out.println("not leap year");
+		}*/
+		/*int m1=78;
+		if(m1>90)
+		{
+			System.out.println("A+");
+		}
+		else if(m1>80)
+		{
+			System.out.println("A");
+		}
+		else if(m1>70)
+		{
+			System.out.println("B");
+		}
+		else
+		{
+			System.out.println("c");
+		}*/
+		/*int i,s=0;
+		for(i=0;i<=10;i++)
+		{
+			s=s+i;
+		}
+		System.out.println(s);*/
+		/*int fact=1,i;
+		for(i=1;i<=5;i++)
+		{
+			fact=fact*i;
+		}
+		System.out.println(fact);*/
+		int i;
+		for(i=2;i<=10;i++)
+		{
+			if(i%2==0)
+			{
+				System.out.println(i);
+			}
+		}
+	}
+
+}
